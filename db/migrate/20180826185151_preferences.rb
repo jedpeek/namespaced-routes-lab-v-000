69a987ec3ff8a3abe812f03857284ae078b1a344
Jed Peek <jedpeek@yahoo.com>
2018-08-26 18:58:10 +0000

    automatically backed up by learn

--- db/migrate/20180826185151_preferences.rb
+++ db/migrate/20180826185151_preferences.rb
@@ -3,8 +3,6 @@ class Preferences < ActiveRecord::Migration
     create_table :preferences do |t|
       t.string :song_sort_order
       t.string :artist_sort_order
-      t.boolean :allow_create_songs
-      t.boolean :allow_create_artists
     end
   end
 end
