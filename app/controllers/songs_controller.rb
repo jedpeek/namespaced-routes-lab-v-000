e5491da1fca44064edb08b2985fca3c44980ce54
Jed Peek <jedpeek@yahoo.com>
2018-08-26 19:16:11 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,8 @@
 class SongsController < ApplicationController
   before_action :set_preferences, only: [:index, :new]
-  
+
   def index
+    @preference
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
