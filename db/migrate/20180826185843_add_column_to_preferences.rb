e88b9cd924d901e9b9b0b2b0e213bf3786fb0fad
Jed Peek <jedpeek@yahoo.com>
2018-08-26 18:59:53 +0000

    automatically backed up by learn

--- db/migrate/20180826185843_add_column_to_preferences.rb
+++ db/migrate/20180826185843_add_column_to_preferences.rb
@@ -2,6 +2,5 @@ class AddColumnToPreferences < ActiveRecord::Migration
   def change
     add_column :preferences, :allow_create_songs, :boolean
     add_column :preferences, :allow_create_artists, :boolean
-    t.boolean
   end
 end
