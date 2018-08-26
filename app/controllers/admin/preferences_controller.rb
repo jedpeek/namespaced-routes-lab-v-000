cb67b36a8af558a8811104bf2c126ed1529dc2a1
Jed Peek <jedpeek@yahoo.com>
2018-08-26 19:09:49 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,4 @@
 class Admin::PreferencesController < ApplicationController
-  def index
-  end
     def index
       @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
     end
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,7 +9,7 @@ class ArtistsController < ApplicationController
 
   def new
     p = Preference.new(allow_create_artists: false)
-    if p.allow_create_artists
+    if @preference.allow_create_artists
       @artist = Artist.new
     else
       redirect_to artists_path
